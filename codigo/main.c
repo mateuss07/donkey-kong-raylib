74af0767f44379b7a67da790c7be621ed3870bd4
Mateus <mateusscrespo@gmail.com>
2026-05-13 18:21:46 -0300

    modified:   .vscode/launch.json 	modified:   .vscode/settings.json 	modified:   .vscode/tasks.json 	modified:   codigo/main.c 	new file:   headers/raylib.h 	new file:   jogo.exe

--- codigo/main.c
+++ codigo/main.c
@@ -1,7 +1,22 @@
-#include <raylib.h>
+#include "raylib.h"
 
-int main(){
 
+int main() {
+
+    InitWindow(800, 450, "Teste");
+
+    while (!WindowShouldClose()) {
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello Raylib!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
