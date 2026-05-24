f7d14e3afbdb55ba5112ab5d8f28a0dfe8fd2517
Mateus <mateusscrespo@gmail.com>
2026-05-24 16:57:58 -0300

    new file:   codigo/bibliotecas/inimigo.c new file:   codigo/bibliotecas/jogador.c new file:   codigo/bibliotecas/mapa.c new file:   codigo/bibliotecas/placar.c modified:   codigo/main.c new file:   headers/inimigo.h new file:   headers/jogador.h new file:   headers/mapa.h new file:   headers/placar.h modified:   jogo.exe

---
+++ codigo/bibliotecas/mapa.c
@@ -0,0 +1,6 @@
+int soma(int a, int b){
+
+    int resultado=0;
+    resultado = a+b;
+    return resultado;
+}
--- codigo/main.c
+++ codigo/main.c
@@ -11,7 +11,7 @@ int main() {
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Hello Raylib!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Hello Raylib!", 390, 200, 20, RED);
 
         EndDrawing();
     }
